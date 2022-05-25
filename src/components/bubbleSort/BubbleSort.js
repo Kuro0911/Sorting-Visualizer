fef958483d4c524a09c959da060d51ef74b79b88
Dhananjai Sharma <dhananjaisharma09@gmail.com>
2022-05-25 16:56:49 +0530

    about section

--- src/components/bubbleSort/BubbleSort.js
+++ src/components/bubbleSort/BubbleSort.js
@@ -174,7 +174,13 @@ const BubbleSort = ({ button }) => {
         })}
       </Container>
       <Box sx={{ width: 200 }}>
-        <text>about</text>
+        <h1>About</h1>
+        <div>
+          Bubble Sort is the simplest sorting algorithm that works by repeatedly
+          swapping the adjacent elements if they are in the wrong order. This
+          algorithm is not suitable for large data sets as its average and worst
+          case time complexity is quite high.
+        </div>
       </Box>
     </BubbleSortWrapper>
   );
