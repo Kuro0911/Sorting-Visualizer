fbf48ffc940819f30f98bf8c04e4e986e3ae46e2
Dhananjai Sharma <dhananjaisharma09@gmail.com>
2022-06-06 14:10:02 +0530

    fixes

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
-import ParticlesComponent from "../src/components/particles/Particles";
 
 export default function Home() {
   return (
